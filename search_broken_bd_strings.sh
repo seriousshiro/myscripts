1f36c9b5454b222633dd635a84914552536f56e9
seriousshiro <108797637+seriousshiro@users.noreply.github.com>
2023-09-28 00:25:59 +0300

    Update search_broken_bd_strings.sh
    исправил опечатки

--- search_broken_bd_strings.sh
+++ search_broken_bd_strings.sh
@@ -13,22 +13,22 @@ SELECT count(*) FROM table_name
 
 # Перебор по тысяче
 for ((i = 0; i <= $count_strings; i= $i+1000)); do
-    check_between_thousand=$(docker exec --user user_name ekd-postgresql psql --dbname $ekd_ca_db_name -c "
+    check_code_between_thousand=$(docker exec --user user_name ekd-postgresql psql --dbname $ekd_ca_db_name -c "
             SELECT * FROM table_name 
             ORDER BY created_date 
             OFFSET $i LIMIT 1000;" 1>/dev/null;
             echo "$?")
             echo "Проверенно - $i"
     # Перебор по сотне
-    if [ "$check_between_thousand" != "0" ]; then 
+    if [ "$check_code_between_thousand" != "0" ]; then 
             for ((k = $i; k <= $i+100; k++))
-            chech_between_handred=$(docker exec --user user_name ekd-postgresql psql --dbname $ekd_ca_db_name -c "
+            check_code_between_handred=$(docker exec --user user_name ekd-postgresql psql --dbname $ekd_ca_db_name -c "
             SELECT * FROM table_name 
             ORDER BY created_date 
             OFFSET $k LIMIT 1000;" 1>/dev/null;
             echo "$?")
-            if [ $chech_between_handred != "0" ]; then
-                # Перебор по каждой записи из сотне
+            if [ $check_code_between_handred != "0" ]; then
+                # Перебор по каждой записи из сотни
                 for ((g = $k; g <= $k+100; g++))
                 do
                     exit_code_number=$(docker exec --user user_name ekd-postgresql psql --dbname $ekd_ca_db_name -c "
